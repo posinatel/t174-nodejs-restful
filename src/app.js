0d917cd19052765e62a71adb2ddb538738a381ff
Inatel POS <edysegura@inatel.br>
2019-11-23 15:17:55 -0200

    Add not found middleware

--- src/app.js
+++ src/app.js
@@ -4,4 +4,7 @@ const app = express();
 const tasksRouter = require('./routes/tasks');
 app.use('/api/tasks', tasksRouter);
 
+const notFound = require('./middleware/not-found');
+app.use(notFound);
+
 module.exports = app;
