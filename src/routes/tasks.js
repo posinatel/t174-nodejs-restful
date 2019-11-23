a2fcf62acbed13db06098ce8ecf654f798b1fbef
Inatel POS <edysegura@inatel.br>
2019-11-23 16:47:57 -0200

    Add authentication for HTTP POST

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (request, response) => {
+const checkAuth = require('../middleware/check-auth');
+
+router.post('/', checkAuth, (request, response) => {
   const data = {
     message: 'Handling HTTP POST request for /api/tasks'
   };
